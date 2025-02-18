a92874fae6f3858a53d5c0b9727e7705e08dd46b
Jesus Diaz <c-jesus.diaz@on24.com>
2025-02-18 12:03:57 +0100

    fix lint issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "@/components/NavBar";
 import Footer from "@/components/Footer";
-import ThemeToggle from "@/components/ThemeToggle";
 import "./globals.css";
 
 const geistSans = Geist({
